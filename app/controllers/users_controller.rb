21f9bf8a71dbfa5f30decd423f91a6c894d63658
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-08-04 22:54:28 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,27 @@
 class UsersController < ApplicationController
 
+    def show
+        # byebug
+        @user = User.find_by(:id => (params[:id]))
+    end
+
     def new
         @user = User.new
     end
 
+   
     def create
         # raise.params
-        @user = User.new(user_params)
-        # byebug
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to users_path(@user)
+
+        user = User.new(user_params)
+
+        if user.save
+            session[:user_id] = user.id
+            redirect_to user_path(user.id)
         else
-            flash[:error] = @user.errors.full_messages.join(', ')
+            flash[:error] = user.errors.full_messages.join(', ')
             render :new
         end
-    end
-
-    def show
-        # byebug
-        @user = User.find(params[:id])
     end
 
     private
